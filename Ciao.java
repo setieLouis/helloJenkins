f6c88d08a8089d715f43c7b6c218dbfde9a2e543
setieLouis <adjams@live.it>
2021-03-04 09:48:01 +0100

    solve main issue

--- Ciao.java
+++ Ciao.java
@@ -1,6 +1,6 @@
 public class Ciao{
 
-	public static void main(String args){
+	public static void main(String[] args){
 		System.out.println("=======================");
 		System.out.println("Ciao e io vengo da JAVA");
 	}
